ccc371bc26c913d7e3007db28c7407dcfe2366c4
Dharmin Shah <shahdk@rose-hulman.edu>
2015-03-03 14:17:37 -0500

    Changed the lables for the pie chart legend

--- src/edu/rosehulman/serg/smellbuster/gui/PieChartGUI.java
+++ src/edu/rosehulman/serg/smellbuster/gui/PieChartGUI.java
@@ -23,28 +23,47 @@ public class PieChartGUI extends JFrame {
 	private StatisticsCalculator statsCalculator;
 	private String title;
 	private Map<String, Color> colorMap;
-	private String noData;
+	private Map<String, String> chartLegend;
+	private final String noData = "No Data Available";
+	private final String extremelyBadCode = "Extremely Bad Code";
+	private final String badCode = "Bad Code";
+	private final String belowAverage = "Below Average";
+	private final String average = "Average";
+	private final String aboveAverage = "Above Average";
+	private final String goodCode = "Good Code";
+	private final String extremelyGoodCode = "Extremely Good Code";
 
 	public PieChartGUI(String title, ResultTableGUI resultTableGUI) {
 		super("Metric Analysis Statistics");
 		this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		this.noData = "No Data Available";
 		this.title = title;
 		this.colorMap = new HashMap<>();
+		this.chartLegend = new HashMap<>();
 		this.statsCalculator = new StatisticsCalculator(resultTableGUI);
 		loadColorMap();
+		loadChartLegend();
 		setContentPane(getChartPanel());
 	}
-	
-	private void loadColorMap(){
-		this.colorMap.put("1", new Color(255, 0, 0));
-		this.colorMap.put("2", new Color(204, 0, 0));
-		this.colorMap.put("3", new Color(159, 95, 0));
-		this.colorMap.put("4", new Color(255, 102, 0));
-		this.colorMap.put("5", new Color(255, 204, 0));
-		this.colorMap.put("6", new Color(95, 159, 0));
-		this.colorMap.put("7", new Color(0, 255, 0));
+
+	private void loadColorMap() {
+		this.colorMap.put(this.extremelyBadCode, new Color(255, 0, 0));
+		this.colorMap.put(this.badCode, new Color(204, 0, 0));
+		this.colorMap.put(this.belowAverage, new Color(159, 95, 0));
+		this.colorMap.put(this.average, new Color(255, 102, 0));
+		this.colorMap.put(this.aboveAverage, new Color(255, 204, 0));
+		this.colorMap.put(this.goodCode, new Color(95, 159, 0));
+		this.colorMap.put(this.extremelyGoodCode, new Color(0, 255, 0));
 		this.colorMap.put(this.noData, Color.DARK_GRAY);
+	}
+
+	private void loadChartLegend() {
+		this.chartLegend.put("1", this.extremelyBadCode);
+		this.chartLegend.put("2", this.badCode);
+		this.chartLegend.put("3", this.belowAverage);
+		this.chartLegend.put("4", this.average);
+		this.chartLegend.put("5", this.aboveAverage);
+		this.chartLegend.put("6", this.goodCode);
+		this.chartLegend.put("7", this.extremelyGoodCode);
 	}
 
 	public JPanel getChartPanel() {
@@ -58,7 +77,7 @@ public class PieChartGUI extends JFrame {
 		Map<Integer, Double> dataMap = this.statsCalculator.getStats();
 		for (int key : dataMap.keySet()) {
 			sum += dataMap.get(key);
-			dataset.setValue("" + key, dataMap.get(key));
+			dataset.setValue(this.chartLegend.get("" + key), dataMap.get(key));
 		}
 		dataset.setValue(this.noData, 100 - sum);
 		return dataset;
@@ -84,7 +103,7 @@ public class PieChartGUI extends JFrame {
 		plot.setNoDataMessage(this.noData);
 		plot.setCircular(false);
 		plot.setLabelGap(0.02);
-		for(String category: this.colorMap.keySet()){
+		for (String category : this.colorMap.keySet()) {
 			plot.setSectionPaint(category, this.colorMap.get(category));
 		}
 
